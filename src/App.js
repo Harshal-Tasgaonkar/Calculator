065feab8a30eba148663db0575894d1a5ac09684
unknown <harshaltasgaonkar06@gmail.com>
2024-08-30 16:34:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Calculator1 from './Components/Calculator1';
+
+function App() {
+  return (
+    <div>
+
+   <Calculator1></Calculator1>
+
+    </div>
+  );
+}
+
+export default App;
